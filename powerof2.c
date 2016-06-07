52c6c78a7410fbc512673ab28a1e6c59d8edbe1c
KrithigaSankar <krithiga.sankar@gmail.com>
2016-06-08 02:52:48 +0530

    Create powerof2.c

---
+++ powerof2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+
+int main(void) {
+	
+	int number;
+	scanf("%d",&number);
+    
+    
+    ((number^2)==)?printf("%d Power of 2",number^2):printf("Not a Power of 2");	
+    
+	return 0;
+}
